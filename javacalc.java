a907330b6b4d4b241ec8453e8879119ca7705bb5
Fallen_C0de <spinthegamer760@gmail.com>
2023-09-19 11:28:29 -0400

    basic calculator functions created

---
+++ javacalc.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class javacalc{
+
+    public static void main(String[]args){
+        
+        System.out.println("Welcome to the Java Calculator");
+
+        //Creating the scanner input
+        Scanner mathinput = new Scanner(System.in);
+
+        //Asking and Gathering User Input
+        System.out.print("Input your equation: ");
+        
+        
+        Double num1input = mathinput.nextDouble();
+        String opinput = mathinput.next();
+        Double num2input = mathinput.nextDouble();
+        double ans = mathcalculation(num1input, opinput, num2input);
+
+        System.out.print(ans);
+    }
+
+    public static double mathcalculation(double num1, String op, double num2){
+        if (op.equals("+")){
+            return (double)num1 + num2;
+        }
+        else if (op.equals("-")){
+            return (double)num1 - num2;
+        }
+        else if (op.equals("*")){
+            return (double)num1*num2;
+        }
+        else if (op.equals("/")){
+            return (double)num1/num2;
+        }
+        else if (op.equals("%")){
+            return (double)num1%num2;
+        }
+        
+
+        return 5.0;
+    }
+
+}
